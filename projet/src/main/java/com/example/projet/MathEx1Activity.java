efb58bb233674a27d90940ed56ba71edb749b7c1
Napium <lombard.ronan@gmail.com>
2014-04-13 20:25:38 +0200

    Exercice de Math1 Comptage avec chrono

--- projet/src/main/java/com/example/projet/MathEx1Activity.java
+++ projet/src/main/java/com/example/projet/MathEx1Activity.java
@@ -1,21 +1,26 @@
 package com.example.projet;
 
+import android.content.Context;
 import android.media.Image;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -25,8 +30,15 @@ import java.util.Random;
 public class MathEx1Activity extends ActionBarActivity {
 
     private TextView counter;
+    private TextView reponse;
+    private TextView temp;
+    private TextView resultat;
+    private TextView score;
+    private LinearLayout vuejeu;
+    private LinearLayout btnRes;
     private FrameLayout counterLayout;
     private GridLayout gridLayout;
+    private Integer j;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +46,49 @@ public class MathEx1Activity extends ActionBarActivity {
         setContentView(R.layout.activity_math_ex1);
 
         counter = (TextView) findViewById(R.id.counter);
+        temp = (TextView) findViewById(R.id.temp);
+        resultat = (TextView) findViewById(R.id.resultat);
+        score = (TextView) findViewById(R.id.score);
+        score.setText("0");
+
+        reponse = (TextView) findViewById(R.id.reponse);
+        reponse.setCursorVisible(false);
+
+        vuejeu = (LinearLayout) findViewById(R.id.vuejeu);
+        btnRes = (LinearLayout) findViewById(R.id.btnRes);
+
         counterLayout = (FrameLayout) findViewById(R.id.layoutCounter);
         gridLayout = (GridLayout) findViewById(R.id.gridLayout);
 
-
+        btnRes.setVisibility(View.INVISIBLE);
         counterLayout.setVisibility(View.VISIBLE);
+        vuejeu.setVisibility(View.INVISIBLE);
         decompte();
+
+        reponse.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                CharSequence str = reponse.getText();
+                Character lastChar = reponse.getText().charAt(str.length() - 1);
+                if (j.toString().equals(lastChar.toString())) {
+                    Integer mavar = Integer.parseInt(score.getText().toString())+1 ;
+                    score.setText(mavar.toString());
+                    jeux();
+                }
+            }
+        });
     }
 
     public void decompte (){
-
-        counter.setText("Pret ?");
 
         new CountDownTimer(3000, 2) {
 
@@ -54,21 +98,65 @@ public class MathEx1Activity extends ActionBarActivity {
 
             public void onFinish() {
                 counterLayout.setVisibility(View.INVISIBLE);
+                vuejeu.setVisibility(View.VISIBLE);
                 jeux();
+                showKeyboard();
+                chrono();
             }
         }.start();
     }
 
+
+    public void chrono (){
+        new CountDownTimer(10000, 1) {
+
+            public void onTick(long millisUntilFinished) {
+                temp.setText(""+ millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                hideKeyboard();
+                reponse.setEnabled(false);
+                vuejeu.setVisibility(View.INVISIBLE);
+                counterLayout.setVisibility(View.VISIBLE);
+                counter.setText(score.getText());
+                if(Integer.parseInt(score.getText().toString()) > 5){
+                    resultat.setText("Bravo !");
+                }
+                else {
+                    resultat.setText("Pas assez Rapide!");
+                }
+                btnRes.setVisibility(View.VISIBLE);
+
+            }
+        }.start();
+    }
+
+    public void showKeyboard() {
+        InputMethodManager imm = (InputMethodManager) this
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(this.getCurrentFocus(),
+                InputMethodManager.SHOW_FORCED);
+    }
+
+    public void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) this
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(),
+                0);
+    }
+
     public void jeux (){
 
+        gridLayout.removeAllViews();
+
         Random r = new Random();
-        int j = r.nextInt(10 - 1) + 1;
+        j = r.nextInt(10 - 1) + 1;
         for(int i=0;i<j;i++){
             ImageView image = new ImageView(this);
             image.setImageResource(R.drawable.lapin);
-            gridLayout.addView(image);
+            gridLayout.addView(image,i);
         }
-
     }
 
     @Override
